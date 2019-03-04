13fc5904f73e4dd5eca81f114281124dbd6f877e
pachristopher <pachristopher@google.com>
2019-03-04 10:44:07 -0800

    Update version requirement in setup.py
    PiperOrigin-RevId: 236682074

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
         # Dependency for multi-processing.
         'joblib>=0.12,<1',
     ],
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,<4',
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['*.so']},
