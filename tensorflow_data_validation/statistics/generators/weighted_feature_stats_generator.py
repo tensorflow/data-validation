a33f3bceaaf6f295a5687814b5fbcd514e182aa7
tf-data-validation-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-01-02 12:15:52 -0800

    Adds WeightedStatsFeatureGenerator for computing stats about weighted features.
    PiperOrigin-RevId: 287874388

---
+++ tensorflow_data_validation/statistics/generators/weighted_feature_stats_generator.py
@@ -0,0 +1,100 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module that computes statistics used to validate weighted features.
+
+Currently, this module generates the following statistics for each
+weighted feature:
+- missing_value: Number of examples missing the value_feature.
+- missing_weight: Number of examples missing the weight_feature.
+- min_weight_length_diff: The minimum of len(weight_feature) -
+len(value_feature).
+- max_weight_length_diff: The maximum of len(weight_feature) -
+len(value_feature).
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+from __future__ import print_function
+
+from tensorflow_data_validation import types
+from tensorflow_data_validation.statistics.generators import stats_generator
+from tensorflow_data_validation.statistics.generators.constituents import count_missing_generator
+from tensorflow_data_validation.statistics.generators.constituents import length_diff_generator
+
+from typing import Any, Dict, Text
+
+from tensorflow_metadata.proto.v0 import schema_pb2
+from tensorflow_metadata.proto.v0 import statistics_pb2
+
+
+class WeightedFeatureStatsGenerator(stats_generator.CompositeStatsGenerator):
+  """Generates statistics for weighted features."""
+
+  def __init__(self,
+               schema: schema_pb2.Schema,
+               name: Text = 'WeightedFeatureStatsGenerator') -> None:
+    constituents = []
+    for weighted_feature in schema.weighted_feature:
+      weight = types.FeaturePath.from_proto(weighted_feature.weight_feature)
+      value = types.FeaturePath.from_proto(weighted_feature.feature)
+      component_paths = [weight, value]
+      constituents.append(length_diff_generator.LengthDiffGenerator(
+          weight, value, required_paths=component_paths))
+      constituents.append(count_missing_generator.CountMissingGenerator(
+          value, required_paths=component_paths))
+      constituents.append(count_missing_generator.CountMissingGenerator(
+          weight, required_paths=component_paths))
+    super(WeightedFeatureStatsGenerator, self).__init__(name, constituents,
+                                                        schema)
+
+  def extract_composite_output(
+      self, accumulator: Dict[Text,
+                              Any]) -> statistics_pb2.DatasetFeatureStatistics:
+    """Populates and returns a stats proto containing custom stats.
+
+    Args:
+      accumulator: The final accumulator representing the global combine state.
+
+    Returns:
+      A DatasetFeatureStatistics proto.
+    """
+    result = statistics_pb2.DatasetFeatureStatistics()
+    for weighted_feature in self._schema.weighted_feature:
+      feature_result = result.features.add(
+          path=types.FeaturePath([weighted_feature.name]).to_proto())
+      weight = types.FeaturePath.from_proto(weighted_feature.weight_feature)
+      value = types.FeaturePath.from_proto(weighted_feature.feature)
+      required_paths = [weight, value]
+
+      weight_count_missing = accumulator[
+          count_missing_generator.CountMissingGenerator.key(
+              weight, required_paths)]
+      feature_result.custom_stats.add(
+          name='missing_weight', num=weight_count_missing)
+
+      value_count_missing = accumulator[
+          count_missing_generator.CountMissingGenerator.key(
+              value, required_paths)]
+      feature_result.custom_stats.add(
+          name='missing_value', num=value_count_missing)
+
+      min_weight_length_diff, max_weight_length_diff = accumulator[
+          length_diff_generator.LengthDiffGenerator.key(
+              weight, value, required_paths)]
+      feature_result.custom_stats.add(
+          name='min_weight_length_diff', num=min_weight_length_diff)
+      feature_result.custom_stats.add(
+          name='max_weight_length_diff', num=max_weight_length_diff)
+    return result
---
+++ tensorflow_data_validation/statistics/generators/weighted_feature_stats_generator_test.py
@@ -0,0 +1,260 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for WeightedFeatureStatsGenerator."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from tensorflow_data_validation import types
+from tensorflow_data_validation.pyarrow_tf import pyarrow as pa
+from tensorflow_data_validation.statistics.generators import weighted_feature_stats_generator
+from tensorflow_data_validation.utils import test_util
+from google.protobuf import text_format
+from tensorflow_metadata.proto.v0 import schema_pb2
+from tensorflow_metadata.proto.v0 import statistics_pb2
+
+
+class WeightedFeatureStatsGeneratorTest(parameterized.TestCase,
+                                        test_util.CombinerStatsGeneratorTest):
+
+  @parameterized.named_parameters(
+      {
+          'testcase_name': 'AllMatching',
+          'batches': [
+              pa.Table.from_arrays(
+                  [pa.array([['a'], ['a', 'b']]),
+                   pa.array([[2], [2, 4]])], ['value', 'weight'])
+          ],
+          'expected_missing_weight': 0.0,
+          'expected_missing_value': 0.0,
+          'expected_min_weight_length_diff': 0.0,
+          'expected_max_weight_length_diff': 0.0
+      }, {
+          'testcase_name': 'AllMatchingMultiBatch',
+          'batches': [
+              pa.Table.from_arrays(
+                  [pa.array([['a'], ['a', 'b']]),
+                   pa.array([[2], [2, 4]])], ['value', 'weight']),
+              pa.Table.from_arrays(
+                  [pa.array([['a'], ['a', 'b']]),
+                   pa.array([[2], [2, 4]])], ['value', 'weight'])
+          ],
+          'expected_missing_weight': 0.0,
+          'expected_missing_value': 0.0,
+          'expected_min_weight_length_diff': 0.0,
+          'expected_max_weight_length_diff': 0.0
+      }, {
+          'testcase_name': 'LengthMismatchPositive',
+          'batches': [
+              pa.Table.from_arrays(
+                  [pa.array([['a'], ['a']]),
+                   pa.array([[2], [2, 4]])], ['value', 'weight'])
+          ],
+          'expected_missing_weight': 0.0,
+          'expected_missing_value': 0.0,
+          'expected_min_weight_length_diff': 0.0,
+          'expected_max_weight_length_diff': 1.0
+      }, {
+          'testcase_name': 'LengthMismatchNegative',
+          'batches': [
+              pa.Table.from_arrays(
+                  [pa.array([['a'], ['a', 'b']]),
+                   pa.array([[2], [2]])], ['value', 'weight'])
+          ],
+          'expected_missing_weight': 0.0,
+          'expected_missing_value': 0.0,
+          'expected_min_weight_length_diff': -1.0,
+          'expected_max_weight_length_diff': 0.0
+      }, {
+          'testcase_name': 'LengthMismatchMultiBatch',
+          'batches': [
+              pa.Table.from_arrays(
+                  [pa.array([['a'], ['a', 'b']]),
+                   pa.array([[], []])], ['value', 'weight']),
+              pa.Table.from_arrays([pa.array([[1], [1, 1]])], ['other'])
+          ],
+          'expected_missing_weight': 0.0,
+          'expected_missing_value': 0.0,
+          'expected_min_weight_length_diff': -2.0,
+          'expected_max_weight_length_diff': -1.0
+      }, {
+          'testcase_name': 'SomePairsMissing',
+          'batches': [
+              pa.Table.from_arrays([
+                  pa.array([['a'], None, ['a', 'b']]),
+                  pa.array([[1, 1], None, [1, 1, 1]])
+              ], ['value', 'weight'])
+          ],
+          'expected_missing_weight': 0.0,
+          'expected_missing_value': 0.0,
+          'expected_min_weight_length_diff': 1.0,
+          'expected_max_weight_length_diff': 1.0
+      }, {
+          'testcase_name': 'EmptyWeights',
+          'batches': [
+              pa.Table.from_arrays([pa.array([['a'], ['a', 'b']])], ['value'])
+          ],
+          'expected_missing_weight': 2.0,
+          'expected_missing_value': 0.0,
+          'expected_min_weight_length_diff': -2.0,
+          'expected_max_weight_length_diff': -1.0
+      }, {
+          'testcase_name': 'EmptyValues',
+          'batches':
+              [pa.Table.from_arrays([pa.array([[1], [1, 2]])], ['weight'])],
+          'expected_missing_weight': 0.0,
+          'expected_missing_value': 2.0,
+          'expected_min_weight_length_diff': 1.0,
+          'expected_max_weight_length_diff': 2.0
+      }, {
+          'testcase_name': 'EmptyWeightsAndValues',
+          'batches': [pa.Table.from_arrays([])],
+          'expected_missing_weight': 0.0,
+          'expected_missing_value': 0.0,
+          'expected_min_weight_length_diff': 0.0,
+          'expected_max_weight_length_diff': 0.0
+      }, {
+          'testcase_name': 'NullWeightArray',
+          'batches': [
+              pa.Table.from_arrays([
+                  pa.array([['a'], ['a', 'b']]),
+                  pa.array([None, None], type=pa.null())
+              ], ['value', 'weight'])
+          ],
+          'expected_missing_weight': 2.0,
+          'expected_missing_value': 0.0,
+          'expected_min_weight_length_diff': -2.0,
+          'expected_max_weight_length_diff': -1.0
+      })
+  def test_single_weighted_feature(self, batches, expected_missing_weight,
+                                   expected_missing_value,
+                                   expected_min_weight_length_diff,
+                                   expected_max_weight_length_diff):
+    schema = text_format.Parse(
+        """
+        weighted_feature {
+          name: 'weighted_feature'
+          feature {
+            step: 'value'
+          }
+          weight_feature {
+            step: 'weight'
+          }
+        }
+        """, schema_pb2.Schema())
+    generator = (
+        weighted_feature_stats_generator.WeightedFeatureStatsGenerator(schema))
+
+    expected_stats = statistics_pb2.FeatureNameStatistics()
+    expected_stats.path.step.append('weighted_feature')
+    expected_stats.custom_stats.add(
+        name='missing_weight', num=expected_missing_weight)
+    expected_stats.custom_stats.add(
+        name='missing_value', num=expected_missing_value)
+    expected_stats.custom_stats.add(
+        name='min_weight_length_diff',
+        num=expected_min_weight_length_diff)
+    expected_stats.custom_stats.add(
+        name='max_weight_length_diff',
+        num=expected_max_weight_length_diff)
+    expected_result = {types.FeaturePath(['weighted_feature']): expected_stats}
+
+    self.assertCombinerOutputEqual(batches, generator, expected_result)
+
+  def test_shared_weight(self):
+    batches = [
+        pa.Table.from_arrays(
+            [pa.array([['a'], ['a', 'b'], ['a']]),
+             pa.array([['x'], ['y'], ['x']]),
+             pa.array([[2], [4], None])],
+            ['value1', 'value2', 'weight'])
+    ]
+    schema = text_format.Parse(
+        """
+        weighted_feature {
+          name: 'weighted_feature1'
+          feature {
+            step: 'value1'
+          }
+          weight_feature {
+            step: 'weight'
+          }
+        }
+        weighted_feature {
+          name: 'weighted_feature2'
+          feature {
+            step: 'value2'
+          }
+          weight_feature {
+            step: 'weight'
+          }
+        }""", schema_pb2.Schema())
+    generator = (
+        weighted_feature_stats_generator.WeightedFeatureStatsGenerator(schema))
+
+    expected_result = {
+        types.FeaturePath(['weighted_feature1']):
+            text_format.Parse(
+                """
+                path {
+                  step: 'weighted_feature1'
+                }
+                custom_stats {
+                  name: 'missing_weight'
+                  num: 1.0
+                }
+                custom_stats {
+                  name: 'missing_value'
+                  num: 0.0
+                }
+                custom_stats {
+                  name: 'min_weight_length_diff'
+                  num: -1.0
+                }
+                custom_stats {
+                  name: 'max_weight_length_diff'
+                  num: 0.0
+                }""", statistics_pb2.FeatureNameStatistics()),
+        types.FeaturePath(['weighted_feature2']):
+            text_format.Parse(
+                """
+                path {
+                  step: 'weighted_feature2'
+                }
+                custom_stats {
+                  name: 'missing_weight'
+                  num: 1.0
+                }
+                custom_stats {
+                  name: 'missing_value'
+                  num: 0.0
+                }
+                custom_stats {
+                  name: 'min_weight_length_diff'
+                  num: -1.0
+                }
+                custom_stats {
+                  name: 'max_weight_length_diff'
+                  num: 0.0
+                }""", statistics_pb2.FeatureNameStatistics())
+    }
+
+    self.assertCombinerOutputEqual(batches, generator, expected_result)
+
+
+if __name__ == '__main__':
+  absltest.main()
