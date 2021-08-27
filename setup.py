da2f8d2b1cc9368988d58d21f4bf2ea1f8422122
tf-data-validation-team <tensorflow-extended-nonhuman@googlegroups.com>
2021-08-27 15:53:20 -0700

    Restricting Beam to use 2.31 as the newer version of Beam==2.32 is having version conflict with google-cloud-bigquery and pyarrow.
    PiperOrigin-RevId: 393444380

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ setup(
     # six, and protobuf) with TF.
     install_requires=[
         'absl-py>=0.9,<0.13',
-        'apache-beam[gcp]>=2.31,<3',
+        'apache-beam[gcp]>=2.31,<2.32',
         # TODO(b/139941423): Consider using multi-processing provided by
         # Beam's DirectRunner.
         'joblib>=0.12,<0.15',  # Dependency for multi-processing.
