e748237ae99b38b4ec0a77147a42c824be380887
tf-data-validation-team <tensorflow-extended-nonhuman@googlegroups.com>
2021-11-29 10:24:38 -0800

    Updating tensorflow, apache_beam version constraint and also updating TF-Serving version constraint for TFX and TFX_BSL.
    PiperOrigin-RevId: 412922038

--- RELEASE.md
+++ RELEASE.md
@@ -9,6 +9,9 @@
 *   BasicStatsGenerator is now responsible for setting the global num_examples.
     This field will no longer be populated at the DatasetFeatureStatistics level
     if default generators are disabled.
+*   Depends on `apache-beam[gcp]>=2.34,<3`.
+*   Depends on
+    `tensorflow>=1.15.2,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,<3`.
 
 ## Known Issues
 
--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ setup(
     # six, and protobuf) with TF.
     install_requires=[
         'absl-py>=0.9,<0.13',
-        'apache-beam[gcp]>=2.32,<3',
+        'apache-beam[gcp]>=2.34,<3',
         # TODO(b/139941423): Consider using multi-processing provided by
         # Beam's DirectRunner.
         'joblib>=0.12,<0.15',  # Dependency for multi-processing.
@@ -186,7 +186,7 @@ setup(
         'protobuf>=3.13,<4',
         'pyarrow>=1,<6',
         'six>=1.12,<2',
-        'tensorflow>=1.15.2,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,<3',
+        'tensorflow>=1.15.2,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,<3',
         'tensorflow-metadata' + select_constraint(
             default='>=1.4,<1.5',
             nightly='>=1.5.0.dev',
