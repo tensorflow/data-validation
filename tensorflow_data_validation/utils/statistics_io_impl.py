6ff13cb61391a3e2c0c488d18c9df0dfed2c44aa
zwestrick <zwestrick@google.com>
2022-05-31 12:23:07 -0700

    externally a noop
    PiperOrigin-RevId: 452111328

--- tensorflow_data_validation/utils/statistics_io_impl.py
+++ tensorflow_data_validation/utils/statistics_io_impl.py
@@ -44,6 +44,10 @@ class StatisticsIOProvider(object):
     """Return files matching the pattern produced by record_sink_impl."""
     raise NotImplementedError
 
+  def file_suffix(self) -> str:
+    """Returns a file suffix (e.g., .tfrecords)."""
+    raise NotImplementedError
+
 
 def get_io_provider(
     file_format: Optional[str] = None) -> StatisticsIOProvider:
@@ -61,26 +65,18 @@ def get_io_provider(
     file_format = 'tfrecords'
   if file_format not in ('tfrecords',):
     raise ValueError('Unrecognized file_format %s' % file_format)
-  return _ProviderImpl(file_format)
+  return _TFRecordProviderImpl()
 
 
-class _ProviderImpl(StatisticsIOProvider):
+class _TFRecordProviderImpl(StatisticsIOProvider):
   """TFRecord backed impl."""
 
-  def __init__(self, file_format: str):
-    self._file_format = file_format
-
   def record_sink_impl(self,
-                       output_path_prefix: str,
-                       file_format: Optional[str] = None) -> beam.PTransform:
-    if self._file_format == 'tfrecords':
-      return beam.io.WriteToTFRecord(
-          output_path_prefix,
-          coder=beam.coders.ProtoCoder(
-              statistics_pb2.DatasetFeatureStatisticsList))
-    else:
-      raise ValueError(
-          'Unrecognized file format %s.' % file_format)
+                       output_path_prefix: str) -> beam.PTransform:
+    return beam.io.WriteToTFRecord(
+        output_path_prefix,
+        coder=beam.coders.ProtoCoder(
+            statistics_pb2.DatasetFeatureStatisticsList))
 
   def glob(self, output_path_prefix) -> Iterator[str]:
     """Returns filenames matching the output pattern of record_sink_impl."""
@@ -91,13 +87,16 @@ class _ProviderImpl(StatisticsIOProvider):
       paths: Iterable[str],
   ) -> Iterator[statistics_pb2.DatasetFeatureStatisticsList]:
     """Provides iterators over tfrecord backed statistics."""
-    if self._file_format == 'tfrecords':
-      iter_fn = tf.compat.v1.io.tf_record_iterator
-    else:
-      raise NotImplementedError('Unrecognized file_format %s' %
-                                self._file_format)
     for path in paths:
-      for record in iter_fn(path):
+      for record in tf.compat.v1.io.tf_record_iterator(path):
         stats_shard = statistics_pb2.DatasetFeatureStatisticsList()
         stats_shard.ParseFromString(record)
         yield stats_shard
+
+  def file_suffix(self) -> str:
+    """Returns a file suffix (e.g., .tfrecords)."""
+    return '.tfrecords'
+
+
+def should_write_sharded():
+  return False
