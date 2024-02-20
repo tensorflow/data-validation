da0f6bbc716dbd48ea7c059295b338189a431c31
tf-data-validation-team <tensorflow-extended-nonhuman@googlegroups.com>
2024-02-20 08:59:18 -0800

    Internal change
    PiperOrigin-RevId: 608623949

--- RELEASE.md
+++ RELEASE.md
@@ -16,6 +16,8 @@
     time, any M1 support for TFDV is experimental and untested.
 *   Bumps the pybind11 version to 2.11.1
 *   Depends on `tensorflow>=2.15.0,<3`.
+*   Depends on `apache-beam[gcp]>=2.53.0,<3`.
+*   Depends on `protobuf>=4.25.2,<5`.
 
 ## Known Issues
 
--- setup.py
+++ setup.py
@@ -175,13 +175,13 @@ setup(
     # six, and protobuf) with TF.
     install_requires=[
         'absl-py>=0.9,<2.0.0',
-        'apache-beam[gcp]>=2.47,<3',
+        'apache-beam[gcp]>=2.53.0,<3',
         # TODO(b/139941423): Consider using multi-processing provided by
         # Beam's DirectRunner.
         'joblib>=1.2.0',  # Dependency for multi-processing.
         'numpy>=1.22.0',
         'pandas>=1.0,<2',
-        'protobuf>=3.20.3,<5',
+        'protobuf>=4.25.2,<5',
         'pyarrow>=10,<11',
         'pyfarmhash>=0.2.2,<0.4',
         'six>=1.12,<2',
