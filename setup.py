b4d79a597fbb699b86bc5ef01e7895bc1677b121
tf-data-validation-team <tensorflow-extended-nonhuman@googlegroups.com>
2024-03-05 13:07:55 -0800

    Switch to PEP 508 style conditional dependencies.
    PiperOrigin-RevId: 612943796

--- setup.py
+++ setup.py
@@ -29,9 +29,6 @@ from distutils.command import build
 # pylint:enable=g-bad-import-order
 
 
-_IS_PY311 = sys.version_info >= (3, 11)
-
-
 class _BuildCommand(build.build):
   """Build everything that is needed to install.
 
@@ -178,13 +175,15 @@ setup(
     # six, and protobuf) with TF.
     install_requires=[
         'absl-py>=0.9,<2.0.0',
-        f'apache-beam[gcp]>={"2.53" if _IS_PY311 else "2.47"},<3',
+        'apache-beam[gcp]>=2.53,<3;python_version>="3.11"',
+        'apache-beam[gcp]>=2.47,<3;python_version<"3.11"',
         # TODO(b/139941423): Consider using multi-processing provided by
         # Beam's DirectRunner.
         'joblib>=1.2.0',  # Dependency for multi-processing.
         'numpy>=1.22.0',
         'pandas>=1.0,<2',
-        f'protobuf>={"4.25.2" if _IS_PY311 else "3.20.3"},<5',
+        'protobuf>=4.25.2,<5;python_version>="3.11"',
+        'protobuf>=3.20.3,<5;python_version<"3.11"',
         'pyarrow>=10,<11',
         'pyfarmhash>=0.2.2,<0.4',
         'six>=1.12,<2',
